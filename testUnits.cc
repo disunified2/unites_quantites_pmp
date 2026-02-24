c02cb1287fb4222582f2c1cd15ce6641a7ffd7c8
tdelaroche <theo.delaroche@edu.univ-fcomte.fr>
2026-02-24 11:05:56 +0100

    Added extra test for units with a bit more exposants

--- testUnits.cc
+++ testUnits.cc
@@ -13,6 +13,15 @@ TEST(basicUnitTest, Metre) {
   EXPECT_EQ(phy::Metre::mole, 0);
   EXPECT_EQ(phy::Metre::candela, 0);
 }
+TEST(basicUnitTest, Volt) {
+  EXPECT_EQ(phy::Volt::metre, 2);
+  EXPECT_EQ(phy::Volt::kilogram, 1);
+  EXPECT_EQ(phy::Volt::second, -3);
+  EXPECT_EQ(phy::Volt::ampere, -1);
+  EXPECT_EQ(phy::Metre::kelvin, 0);
+  EXPECT_EQ(phy::Metre::mole, 0);
+  EXPECT_EQ(phy::Metre::candela, 0);
+}
 
 
 int main(int argc, char* argv[]) {
