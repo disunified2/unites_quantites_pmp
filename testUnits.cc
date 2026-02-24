d8161710bb769768a31983aad5c2c772fc72cf08
tdelaroche <theo.delaroche@edu.univ-fcomte.fr>
2026-02-24 11:03:11 +0100

    Initial test to make sure

--- testUnits.cc
+++ testUnits.cc
@@ -4,6 +4,17 @@
 
 #include <gtest/gtest.h>
 
+TEST(basicUnitTest, Metre) {
+  EXPECT_EQ(phy::Metre::metre, 1);
+  EXPECT_EQ(phy::Metre::kilogram, 0);
+  EXPECT_EQ(phy::Metre::second, 0);
+  EXPECT_EQ(phy::Metre::ampere, 0);
+  EXPECT_EQ(phy::Metre::kelvin, 0);
+  EXPECT_EQ(phy::Metre::mole, 0);
+  EXPECT_EQ(phy::Metre::candela, 0);
+}
+
+
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
